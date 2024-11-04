b381c5dd2940d5934098ad452c056be1051b9b66
Mhammad jrad <131681662+mhammadjrad@users.noreply.github.com>
2024-11-04 10:53:39 +0300

    Add files via upload

---
+++ md/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void d(int *n, int len )
+{
+   for(int i=0; i<len; i++)
+    cout<<*(n+i)<<"  ";
+    cout<<endl;
+}
+int main()
+{ int a[5];
+    d(a,5);
+
+  int *p=a;
+
+  p+=2;
+  *p=3;
+
+  d(a,5);
+    return 0;
+}
